864503236f8331f437086cdf6be0ae33252b5ef7
Abdulrazzaqaminu <91573374+Abdulrazzaqaminu@users.noreply.github.com>
2021-12-13 17:29:56 +0100

    Update index-speech.js

--- index-speech.js
+++ index-speech.js
@@ -1,4 +1,4 @@
-responsiveVoice.speak("Good day. Welcome to i transactions. Press Enter key for more information, and press arrow down key for the navigation bar");
+responsiveVoice.speak("Good day. Welcome to i transactions. Press Enter key for more information and press arrow down key for the navigation bar");
 window.addEventListener("keyup", e=>{
     if(e.keyCode === 13){
         var text = document.getElementById("intro").textContent;
